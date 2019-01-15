243af4c43774c3448c31c5d5c40a4732aa8c8d5a
Negar Amiri <negamiri@gmail.com>
2019-01-15 21:59:11 +0000

    Display revenue for all stores, average revenue, and high generating stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+puts "Total revenue(for all stores): #{@total_revenue}"
+
+@average_revenue = Store.average('annual_revenue').to_i
+puts "Average revenue(for all stores): #{@average_revenue}"
+
+@high_generating_stores = Store.where('annual_revenue >= 1000000').count
+puts "Number of stores generating $1m or more: #{@high_generating_stores}"
