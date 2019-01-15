371c9b50e1a93816f60bb39e8af2c60619013911
Negar Amiri <negamiri@gmail.com>
2019-01-15 22:21:30 +0000

    Add employees to employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+store1_employees = @store1.employees.create([
+  {first_name: "Negar", last_name: "Amiri", hourly_rate: 50},
+  {first_name: "Xavie", last_name: "Goff", hourly_rate: 50},
+  {first_name: "Taylan", last_name: "Hines", hourly_rate: 60},
+])
+
+store2_employees = @store2.employees.create([
+  {first_name: "Ayaan", last_name: "Michael", hourly_rate: 40},
+  {first_name: "Krish", last_name: "Hensley", hourly_rate: 60},
+  {first_name: "Kamile", last_name: "Bowers", hourly_rate: 60},
+])
